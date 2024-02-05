de40c4ece379aabda8446a2e4b83c86f7b8c50b7
Szilagyimarton <szilagyimarton92@gmail.com>
2024-02-05 16:56:51 +0100

    added github links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,25 +7,24 @@ function Projects() {
     <section id='projects'>
       <h1>Projects</h1>
       <div className="projectsContainer">
-      <div className="imageGallery">
+      <div className="project">
         <h3>To Do App</h3>
-        <img src='src\img\todoapp\todo4.png' alt='todoImg' />
+        <Link to="https://github.com/Szilagyimarton/todo-app/tree/main" target="_blank">
+          <img src='src\img\todoapp\todo4.png' alt='todoImg' />
+        </Link>
         <p>This project made with <span className='emphasize'>React</span>, uses <span className='emphasize'>Firebase</span> authentication, Firebase database and <span className='emphasize'>Material UI</span>.</p>
         <p>After registration and login you can add tasks, set the priority and also able to delete the existing ones.</p>
       </div>
-      <div className="imageGallery">
+      <div className="project">
         <h3>Pokemon Game</h3>
         <img src='src\img\pokemon\poke1.png' alt='pokemonImg' />
         <p>This one is a simple game uses the PokéAPI. It made with <span className='emphasize'>React</span>, the design made with  mostly simple CSS and a little Material UI.</p>
       </div>
-      <div className="imageGallery">
-        <h3>Age calculator app</h3>
-        <img src='src\img\agecalc\agecalc2_thumb.jpg' alt='ageCalcImg' />
-        <p>This is a simple age calculator application made by <span className='emphasize'>Javascript, HTML, CSS.</span> After you've give your birth date it will calculate the exact age of yours.</p>
-      </div>
-      <div className="imageGallery">
+      <div className="project">
         <h3>Recipe Website</h3>
+        <Link to="https://github.com/Szilagyimarton/react-recipe-app" target='_blank'>
         <img src='src\img\recipeAPI\recipe4.png' alt='recipeAppImg' />
+        </Link>
         <p>This project is uses the MealDB API. You can search for recipes, or show them by category or area. It made with <span className='emphasize'>React</span>. For routing i used the <span className='emphasize'>react-router</span> and for the design the <span className='emphasize'>Material UI.</span> </p>
       </div>
       </div>
